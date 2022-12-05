1d44c4d581b2fc607067d0ddd60712880c156144
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2022-12-05 13:14:48 -0300

    function calcularImc final
    realiza o Calculo IMC e retornando resultado para tela

--- _js/script.js
+++ _js/script.js
@@ -7,8 +7,8 @@ function validarInputEmpty(id) {
             document.getElementById(id).className = c + " " + 'is-invalid';
             result = false;
         }
-    } else { 
-        if(valor == "" || valor <= 0) {
+    } else {
+        if (valor == "" || valor <= 0) {
             document.getElementById(id).className = c + " " + 'is-invalid';
             result = false;
         }
@@ -45,15 +45,74 @@ function calcularImc(nome, idade, altura, peso) {
     var indice = new Array(nome, idade, altura, peso);
     var cont = 0;
     for (const i in indice) {
-        if(!validarInputEmpty(indice[i])) {
+        if (!validarInputEmpty(indice[i])) {
             cont++;
         }
     }
-    if(cont > 0) {
+    if (cont > 0) {
         document.getElementById("fail").innerHTML = "<div class='alert alert-danger alert-dismissible mt-2'><button class='close' type='button' data-dismiss='alert'> &times; </button>Por favor revise os dados digitados!</div>";
+    } else {
+        peso = parseFloat(document.getElementById(peso).value);
+        altura = parseFloat(document.getElementById(altura).value / 100);
+        let imc = peso / (Math.pow(altura, 2));
 
-        // document.getElementById("fail").innerHTML = "teste";
+        let sex = new Array();
+        sex = document.getElementsByName("sex");
+        let cont = 0;
+        let vsex = '';
+        while (cont < sex.length) {
+            if (sex[cont].checked) {
+                vsex = sex[cont].value;
+            }
+            cont++;
+        }
+
+
+        if(vsex == "m") {
+            document.getElementById("renome").innerHTML = "Olá " + document.getElementById(nome).value;
+            document.getElementById("recalculo").innerHTML = "Seu cálculo IMC é de: <strong>" + imc + "</strong> (Masculino)";
+
+            if(imc < 20.7) {
+                document.getElementById("reimc").innerHTML = "Você encontra-se na categoria: <strong> Abaixo do peso </strong>"
+            } else {
+                if(imc >= 20.7 && imc <= 26.4) {
+                    document.getElementById("reimc").innerHTML = "Você encontra-se na categoria: <strong> Peso ideal </strong>"
+                } else {
+                    if(imc > 26.4 && imc <= 27.8) {
+                        document.getElementById("reimc").innerHTML = "Você encontra-se na categoria: <strong> Pouco acima do peso </strong>"
+                    } else {
+                        if(imc > 27.8 && imc <= 31.1) {
+                            document.getElementById("reimc").innerHTML = "Você encontra-se na categoria: <strong> Acima do peso </strong>"
+                        } else {
+                            document.getElementById("reimc").innerHTML = "Você encontra-se na categoria: <strong> Obesidade </strong>"
+                        }
+                    }
+                }
+            }
+        }
+
+
+        if(vsex == "f") {
+            document.getElementById("renome").innerHTML = "Olá " + document.getElementById(nome).value;
+            document.getElementById("recalculo").innerHTML = "Seu cálculo IMC é de: <strong>" + imc + "</strong> (Feminino)";
+
+            if(imc < 19.1) {
+                document.getElementById("reimc").innerHTML = "Você encontra-se na categoria: <strong> Abaixo do peso </strong>"
+            } else {
+                if(imc >= 19.1 && imc <= 25.8) {
+                    document.getElementById("reimc").innerHTML = "Você encontra-se na categoria: <strong> Peso ideal </strong>"
+                } else {
+                    if(imc > 25.8 && imc <= 27.3) {
+                        document.getElementById("reimc").innerHTML = "Você encontra-se na categoria: <strong> Pouco acima do peso </strong>"
+                    } else {
+                        if(imc > 27.3 && imc <= 32.3) {
+                            document.getElementById("reimc").innerHTML = "Você encontra-se na categoria: <strong> Acima do peso </strong>"
+                        } else {
+                            document.getElementById("reimc").innerHTML = "Você encontra-se na categoria: <strong> Obesidade </strong>"
+                        }
+                    }
+                }
+            }
+        }
     }
-    console.log(cont);
-
 }
